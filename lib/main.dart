14f4d7fcb7cb685111e25bdc4022eda865ee8c10
RajuHaldar <rajuhaldarx@gmail.com>
2023-08-11 08:58:58 +0100

    ll

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:car_booking/booking.dart';
 import 'package:car_booking/profile.dart';
+import 'package:car_booking/rloginn.dart';
 import 'package:car_booking/sign_in.dart';
 import 'package:car_booking/front.dart';
 import 'package:car_booking/lorry.dart';
@@ -15,8 +16,7 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Login());
+    return const MaterialApp(home: RLogin());
   }
-  //add data
-  //wertyu
+
 }
