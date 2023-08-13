d9de9b6c33b87bea414970aecceca915ea345524
deepalikewat <kewatdeepali6@gmail.com>
2023-08-13 10:36:31 +0530

    lo

--- lib/main.dart
+++ lib/main.dart
@@ -9,19 +9,16 @@ import 'package:car_booking/lorry.dart';
 import 'package:car_booking/otp_verify.dart';
 import 'package:flutter/material.dart';
 
-import 'map_with_autocomplete.dart';
-
 void main() {
   runApp(const MainApp());
 }
 //uuc
-
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: OtpVeri());
+    return MaterialApp(home: Booking());
   }
 }
