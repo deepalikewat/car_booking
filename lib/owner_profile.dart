8a3b709b6492e429d22f1d2df65a59c3398cdfc1
deepalikewat <kewatdeepali6@gmail.com>
2023-08-13 15:32:23 +0530

    ico

--- lib/owner_profile.dart
+++ lib/owner_profile.dart
@@ -12,78 +12,70 @@ class rix extends State<Owner> {
   @override
   Widget build(BuildContext context) {
     double xwidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(""),
-              Text(
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(children: [
+              const Padding(padding: EdgeInsets.only(top: 10)),
+              const Text(
                 "Owner Profile",
                 style: TextStyle(
                     fontSize: 20,
                     fontWeight: FontWeight.bold,
                     color: Color(0xff1B1E28)),
               ),
-              Row(
-                children: [
-                  Text(
-                    "Done",
-                    style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        color: Color(0xff0D6EFD)),
-                  ),
-                ],
-              ),
-            ]),
-      ),
-      body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Column(children: [
-              const Padding(padding: EdgeInsets.only(top: 50)),
               Container(
-                decoration:
-                    BoxDecoration(shape: BoxShape.circle, border: Border.all()),
-                height: 120,
-                width: 122,
-                child: const ClipOval(
-                    child: Image(
-                  image: AssetImage("img/QT.jpeg"),
-                  width: 100,
-                  height: 300,
-                  fit: BoxFit.cover,
-                )),
+                height: 130,
+                width: 130,
+
+                child: Stack(children: [
+                  const ClipOval(
+                      child: Image(
+                    image: AssetImage("img/QT.jpeg"),
+                    width: 130,
+                    height: 130,
+                    fit: BoxFit.cover,
+                  )),
+                  Positioned(
+                      bottom: 0,
+                      right: 0,
+                      child: IconButton(
+                        style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStatePropertyAll(Color(0xff2EA7D4)),
+                        ),
+                        icon: const Icon(Icons.camera_alt),
+                        onPressed: () {},
+                      ))
+
+                  // child: Icon(
+
+                  //   Icons.camera,
+                  //   color: Color(0xff2EA7D4),
+                  //   size: 30,
+                  // ))
+                ]),
+
+                // child: const ClipOval(
+                //     child: Image(
+                //   image: AssetImage("img/QT.jpeg"),
+                //   width: 100,
+                //   height: 300,
+                //   fit: BoxFit.cover,
+                // )),
               ),
               const Padding(padding: EdgeInsets.only(top: 20)),
-              // const Padding(padding: EdgeInsets.only(top: 20, bottom: 10)),
-              // const Text(
-              //   "Cutest",
-              //   style: TextStyle(
-              //       fontSize: 24,
-              //       fontWeight: FontWeight.w400,
-              //       color: Color(0xff1B1E28)),
-              // ),
-
-              // const Padding(
-              //   padding: EdgeInsets.only(top: 8, bottom: 50),
-              //   child: Text(
-              //     "Change Profile Picture",
-              //     style: TextStyle(fontSize: 16, color: Color(0xff0D6EFD)),
-              //   ),
-              // ),
-
               Container(
-                padding: EdgeInsets.only(bottom: 8),
+                padding: const EdgeInsets.only(bottom: 8),
                 width: xwidth - 70,
                 child: const Text(
                   "Full  Name",
                   style: TextStyle(fontSize: 18, color: Color(0xff1B1E28)),
                 ),
               ),
-
               Container(
                 width: xwidth - 60,
                 child: const TextField(
@@ -95,16 +87,14 @@ class rix extends State<Owner> {
                   ),
                 ),
               ),
-
               Container(
-                padding: EdgeInsets.only(bottom: 8, top: 20),
+                padding: const EdgeInsets.only(bottom: 8, top: 20),
                 width: xwidth - 70,
                 child: const Text(
                   "Enter Number",
                   style: TextStyle(fontSize: 18, color: Color(0xff1B1E28)),
                 ),
               ),
-
               Container(
                 width: xwidth - 60,
                 child: const TextField(
@@ -116,16 +106,14 @@ class rix extends State<Owner> {
                   ),
                 ),
               ),
-
               Container(
-                padding: EdgeInsets.only(bottom: 8, top: 20),
+                padding: const EdgeInsets.only(bottom: 8, top: 20),
                 width: xwidth - 70,
                 child: const Text(
                   "Email Id",
                   style: TextStyle(fontSize: 18, color: Color(0xff1B1E28)),
                 ),
               ),
-
               Container(
                 width: xwidth - 60,
                 child: const TextField(
@@ -137,9 +125,8 @@ class rix extends State<Owner> {
                   ),
                 ),
               ),
-
               Container(
-                padding: EdgeInsets.only(bottom: 8, top: 20),
+                padding: const EdgeInsets.only(bottom: 8, top: 20),
                 width: xwidth - 70,
                 child: const Text(
                   "Full Address",
@@ -147,7 +134,7 @@ class rix extends State<Owner> {
                 ),
               ),
               Container(
-                padding: EdgeInsets.only(bottom: 200),
+                padding: const EdgeInsets.only(bottom: 8),
                 width: xwidth - 60,
                 child: const TextField(
                   style: TextStyle(fontSize: 16),
@@ -158,18 +145,16 @@ class rix extends State<Owner> {
                   ),
                 ),
               ),
-
               Container(
-                padding: EdgeInsets.only(bottom: 8, top: 20),
+                padding: const EdgeInsets.only(bottom: 8, top: 20),
                 width: xwidth - 70,
                 child: const Text(
                   "Aadhar Card Number",
                   style: TextStyle(fontSize: 18, color: Color(0xff1B1E28)),
                 ),
               ),
-
               Container(
-                padding: EdgeInsets.only(bottom: 200),
+                // padding: const EdgeInsets.only(bottom: ),
                 width: xwidth - 60,
                 child: const TextField(
                   style: TextStyle(fontSize: 16),
@@ -180,9 +165,9 @@ class rix extends State<Owner> {
                   ),
                 ),
               ),
-
+              const Expanded(child: Text("")),
               Container(
-                padding: EdgeInsets.symmetric(horizontal: 20),
+                padding: const EdgeInsets.symmetric(horizontal: 20),
                 height: 60,
                 width: xwidth,
                 child: ElevatedButton(
@@ -191,12 +176,15 @@ class rix extends State<Owner> {
                       backgroundColor: const Color(0xff0D6EFD),
                       shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(16))),
-                  child: Text(
+                  child: const Text(
                     "Next",
                     style: TextStyle(fontSize: 20),
                   ),
                 ),
               ),
+              const SizedBox(
+                height: 20,
+              )
             ])
           ],
         ),
