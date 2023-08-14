81ebccd598f83a10b880a206cd0ebf95d7d7669e
deepalikewat <kewatdeepali6@gmail.com>
2023-08-14 16:17:33 +0530

    ll

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:car_booking/booking.dart';
+import 'package:car_booking/driver_profile.dart';
+import 'package:car_booking/owner_profile.dart';
+import 'package:car_booking/profile.dart';
+import 'package:car_booking/rloginn.dart';
+import 'package:car_booking/front.dart';
+import 'package:car_booking/lorry.dart';
+import 'package:car_booking/otp_verify.dart';
+import 'package:flutter/material.dart';
+
+import 'map_with_autocomplete.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+//uuc
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MapScreen());
+  }
+}
