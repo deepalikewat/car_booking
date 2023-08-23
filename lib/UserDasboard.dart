0cb5815b7712df0c35faacd8eec9c1891611e040
RajuHaldar <rajuhaldarx@gmail.com>
2023-08-23 08:07:49 +0100

    xx

--- lib/UserDasboard.dart
+++ lib/UserDasboard.dart
@@ -1,12 +1,10 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
-import 'package:http/http.dart' as http;
-
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_maps_webservice_ex/places.dart';
 import 'package:map_autocomplete_field/map_autocomplete_field.dart';
+
 
 class UserDashBoard extends StatefulWidget {
   const UserDashBoard({super.key});
@@ -17,172 +15,16 @@ class UserDashBoard extends StatefulWidget {
 
 }
 
-class GeocodingService {
-  static const String apiKey = 'AIzaSyCFAovotUynUVLXytFxhkulPpviRDJQjtI';
-
-  Future<Map<String, dynamic>> getLocationData(String address) async {
-    final response = await http.get(Uri.parse(
-        'https://maps.googleapis.com/maps/api/geocode/json?address=$address&key=$apiKey'));
-
-    if (response.statusCode == 200) {
-      final data = jsonDecode(response.body);
-      final results = data['results'];
-      if (results.isNotEmpty) {
-        final location = results[0]['geometry']['location'];
-        final latitude = location['lat'];
-        final longitude = location['lng'];
-        final components = results[0]['address_components'];
-        String? pincode;
-
-        for (var component in components) {
-          final types = component['types'];
-          if (types.contains('postal_code')) {
-            pincode = component['long_name'];
-            break;
-          }
-        }
-
-        if (pincode != null) {
-          return {
-            'latitude': latitude,
-            'longitude': longitude,
-            'pincode': pincode,
-          };
-        } else {
-          final nearestPincode = await getNearestPincode(latitude, longitude);
-          return {
-            'latitude': latitude,
-            'longitude': longitude,
-            'pincode': nearestPincode,
-          };
-        }
-      } else {
-        throw Exception('No results found for the address');
-      }
-    } else {
-      throw Exception('Request failed with status: ${response.statusCode}');
-    }
-  }
-
-  Future<String> getNearestPincode(double latitude, double longitude) async {
-    final response = await http.get(Uri.parse(
-        'https://maps.googleapis.com/maps/api/geocode/json?latlng=$latitude,$longitude&key=$apiKey'));
-
-    if (response.statusCode == 200) {
-      final data = jsonDecode(response.body);
-      final results = data['results'];
-      if (results.isNotEmpty) {
-        final components = results[0]['address_components'];
-        for (var component in components) {
-          final types = component['types'];
-          if (types.contains('postal_code')) {
-            return component['long_name'];
-          }
-        }
-      }
-    }
-
-    throw Exception('No nearest pincode found');
-  }
-}
-
-class PlacesApi {
-  static const String apiKey = 'AIzaSyCFAovotUynUVLXytFxhkulPpviRDJQjtI';
-
-  static Future<List<Prediction>> getPlacePredictions(String input) async {
-    const apiKeyParam = 'key=$apiKey';
-    final inputParam = 'input=${Uri.encodeQueryComponent(input)}';
-    const typesParam = 'types=address';
-    const languageParam = 'language=en';
-
-    final endpoint =
-        'https://maps.googleapis.com/maps/api/place/autocomplete/json?$inputParam&$typesParam&$languageParam&$apiKeyParam';
-
-    final response = await http.get(Uri.parse(endpoint));
-    if (response.statusCode == 200) {
-      final predictions = jsonDecode(response.body)['predictions'] as List;
-      return predictions
-          .map((prediction) => Prediction.fromJson(prediction))
-          .toList();
-    } else {
-      throw Exception('Failed to fetch place predictions');
-    }
-  }
-}
-
-class Prediction {
-  final String description;
-  Prediction({required this.description});
-
-  factory Prediction.fromJson(Map<String, dynamic> json) {
-    return Prediction(description: json['description']);
-  }
-}
-
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 class UserDashBoardx  extends State<UserDashBoard>{
-
-
-
 
   GoogleMapController? _mapController;
   final TextEditingController _sourceController = TextEditingController();
   final TextEditingController _destinationController = TextEditingController();
   final apiKey ='AIzaSyDXLXII5-jnC-fJ5hSF3xc5ucf_O_ecOfQ';
-  late LatLng _sourceLatLng=LatLng(21.1290, 82.7792);
-  late LatLng _destinationLatLng=LatLng(21.1280, 82.7792);
+  late LatLng _sourceLatLng=const LatLng(21.1290, 82.7792);
+  late LatLng _destinationLatLng=const LatLng(21.1280, 82.7792);
   TextEditingController controller = TextEditingController();
-
-
-
-
-Future<List<String>> getSuggestedAddresses(String pattern) async {
-    final predictions = await PlacesApi.getPlacePredictions(pattern);
-
-    final suggestions =
-        predictions.map((prediction) => prediction.description).toList();
-
-    return suggestions;
-  }
-
-  double screenxp = 2.7;
-
-
-
-
-
-  String distancex = "0.00 km";
-  String farex = "Fare";
-  Set<Marker> markers = Set();
-
-  List<LatLng> _polylineCoordinates = [];
-  Set<Polyline> _polylines = {};
-
-
-
-
-
-
-
-
-
+  final GoogleMapsPlaces _place = GoogleMapsPlaces(apiKey: "AIzaSyDXLXII5-jnC-fJ5hSF3xc5ucf_O_ecOfQ" );
 
   @override
   Widget build(BuildContext context) {
@@ -233,11 +75,11 @@ SizedBox(height: xheight,),
               },
               markers: {
                 Marker(
-                  markerId: MarkerId("source"),
+                  markerId: const MarkerId("source"),
                   position: _sourceLatLng,
                 ),
                 Marker(
-                  markerId: MarkerId("destination"),
+                  markerId: const MarkerId("destination"),
                   position: _destinationLatLng,
                 ),
               },
@@ -268,8 +110,6 @@ borderRadius: BorderRadius.only(topLeft: Radius.circular(
             
             SizedBox(height: xheight*.03,),
             
-
-
 
 MapAutoCompleteField(
 
@@ -302,8 +142,11 @@ MapAutoCompleteField(
                 title: Text(suggestion.description),
               );
             },
-            onSuggestionSelected: (suggestion) {
+            onSuggestionSelected: (suggestion) async {
               srcctl.text = suggestion.description;
+
+
+ 
             },
           
             inputDecoration: const InputDecoration(
@@ -315,7 +158,7 @@ MapAutoCompleteField(
             ,
             const SizedBox(height: 10,),
 
-            TextField(
+            const TextField(
 
 decoration: InputDecoration(hintText: "Product Type",prefixIcon: Icon(Icons.shopping_cart,color: Color(0xff0c50b4))),
             ),
@@ -326,7 +169,7 @@ decoration: InputDecoration(hintText: "Product Type",prefixIcon: Icon(Icons.shop
             controller: _controllerdx,
            onTap: (){_selectDate(context);
                   },
-decoration: InputDecoration(prefixIcon: Icon(Icons.calendar_month_outlined,color: Color(0xff0c50b4)),
+decoration: const InputDecoration(prefixIcon: Icon(Icons.calendar_month_outlined,color: Color(0xff0c50b4)),
 hintText: "PickUp date"
 ),
 
@@ -334,7 +177,7 @@ hintText: "PickUp date"
 ,
             const SizedBox(height: 10,),
 
-TextField(
+const TextField(
   keyboardType: TextInputType.number,
   decoration: InputDecoration(
     hintText: "Offer Your Fare",
@@ -347,7 +190,7 @@ TextField(
             
             ,
 
-            Expanded(child: SizedBox()),
+            const Expanded(child: SizedBox()),
 
  Container(
               padding: const EdgeInsets.only(right: 10,left: 10,bottom: 10),
