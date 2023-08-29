acfc84e2fea0939fa8650308c2ab6b02f72ab6b5
RajuHaldar <rajuhaldarx@gmail.com>
2023-08-29 17:39:10 +0100

    gg

---
+++ lib/UploadDashboard.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class UploadDashboard extends StatefulWidget {
+  @override
+  State<UploadDashboard> createState() => updash();
+}
+
+class updash extends State<UploadDashboard> {
+  
+   void dinc(BuildContext context,String title,) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+              content: Container(
+              )
+          );
+        }
+    );
+   }
+
+  @override
+  Widget build(BuildContext context) {
+ 
+ 
+
+ return  Scaffold(
+
+
+body: Center(
+  child:   Column(children: [
+  
+  
+    
+    Container(
+      child: Row(children: [
+    
+    
+    ElevatedButton(onPressed:()=> dinc(context,"adhar card"), child:
+    
+     )
+    
+    
+      ],),
+    )
+  
+  ]),
+),
+
+ );
+
+
+  }
+
+
+
+
+}
+
