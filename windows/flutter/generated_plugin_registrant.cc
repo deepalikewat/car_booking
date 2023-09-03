387a0b8426b0c6e94e05fb381f471ad0c1fd9672
RajuHaldar <rajuhaldarx@gmail.com>
2023-09-03 09:55:25 +0100

    c

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -7,12 +7,15 @@
 #include "generated_plugin_registrant.h"
 
 #include <file_selector_windows/file_selector_windows.h>
+#include <geolocator_windows/geolocator_windows.h>
 #include <map_autocomplete_field/map_autocomplete_field_plugin_c_api.h>
 #include <smart_auth/smart_auth_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   FileSelectorWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FileSelectorWindows"));
+  GeolocatorWindowsRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("GeolocatorWindows"));
   MapAutocompleteFieldPluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("MapAutocompleteFieldPluginCApi"));
   SmartAuthPluginRegisterWithRegistrar(
