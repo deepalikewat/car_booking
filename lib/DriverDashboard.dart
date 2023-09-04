34936732d6825f4d8073d6583439158c75827a4a
RajuHaldar <rajuhaldarx@gmail.com>
2023-09-04 13:19:27 +0100

    x

--- lib/DriverDashboard.dart
+++ lib/DriverDashboard.dart
@@ -1,12 +1,17 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:url_launcher/url_launcher.dart';
+<<<<<<< HEAD
 
 import 'dart:ui';
 
+=======
+import 'package:http/http.dart' as http;
+>>>>>>> e9e1115 (x)
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 
@@ -21,9 +26,13 @@ class DriverDashBoard extends StatefulWidget {
 class DriverDashBoardx extends State<DriverDashBoard> {
 
 
+
+
   List<Map<String, dynamic>> bookingData = [
 
   ];
+
+
 
   //   {
   //     "request_booking_id": 1,
@@ -144,35 +153,82 @@ Future<Position> _determinePosition() async {
   Future<void> rtvv() async {
 
 
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    final dc = await http.post(
+        Uri.parse("https://admin.returnlorry.com/appservice/getbookingrequest"),
+        body: jsonEncode({ 
+    "Token": prefs.getString("Token"),
+    "userId": prefs.getString("userId"),
+    "userPhone": prefs.getString("userPhone"),
+    "userType": prefs.getString("userType"),
+    }
+        ));
 
 
+
+
+
+final rty=jsonDecode(dc.body)["data"]["data"];
+
+int i =0;
+bookingData.clear();
+
+for (var ty in rty){
+
+i++;
+
+bookingData.add(ty);
+
+}
+
+
+
+
+
+setState(() {
+  
+        _isExpandedList = List.generate(bookingData.length, (index) => false);
+
+});
+
+
+
+// setState(() {
+
+//       _isExpandedList = List.generate(bookingData.length, (index) => false);
+
+
+
+//   print("raju");
+// });
     // Map<String, dynamic> ryo={
-      "request_booking_id": 1,
-      "booking_user": {
-        "id": 1,
-        "name": "Raju",
-        "dp": "https://autohyre.com/assets/images/output-onlinepngtools.png"
-      },
-      "pick_up_point": {
-        "lat": "24.185764",
-        "long": "87.791982",
-        "pincode": "713148",
-        "address": "sdfsdf,sdf,sd,fdsf,d,f"
-      },
-      "destination_point": {
-        "lat": "22.568660563718094",
-        "long": "88.51089299422036",
-        "pincode": "713148",
-        "address": "sds,dfsdf,fdsdf"
-      },
-      "vehicle_type_id": 1,
-      "material_weight": 2000,
-      "material_type": "Plastic",
-      "distance": 250,
-      "calculated_price": 25000,
-      "booking_date": "2023-05-20 12:31:57",
-      "request_status": {"status_code": "0", "status": "Open"}
-    };
+    //   "request_booking_id": 1,
+    //   "booking_user": {
+    //     "id": 1,
+    //     "name": "Raju",
+    //     "dp": "https://autohyre.com/assets/images/output-onlinepngtools.png"
+    //   },
+    //   "pick_up_point": {
+    //     "lat": "24.185764",
+    //     "long": "87.791982",
+    //     "pincode": "713148",
+    //     "address": "sdfsdf,sdf,sd,fdsf,d,f"
+    //   },
+    //   "destination_point": {
+    //     "lat": "22.568660563718094",
+    //     "long": "88.51089299422036",
+    //     "pincode": "713148",
+    //     "address": "sds,dfsdf,fdsdf"
+    //   },
+    //   "vehicle_type_id": 1,
+    //   "material_weight": 2000,
+    //   "material_type": "Plastic",
+    //   "distance": 250,
+    //   "calculated_price": 25000,
+    //   "booking_date": "2023-05-20 12:31:57",
+    //   "request_status": {"status_code": "0", "status": "Open"}
+    // };
     
 
 
@@ -565,13 +621,12 @@ _driver_email,
                     
                       leading: CircleAvatar(
                     
-                        backgroundImage:
-                    
-                            NetworkImage(booking['booking_user']['dp']),
+                        backgroundImage: (booking['booking_user']['image']=="https://admin.returnlorry.com/uploads/users/") ? const AssetImage("img/dp.png") as ImageProvider<Object>:
+                            NetworkImage(booking['booking_user']['image']),
                     
                       ),
                     
-                      title: Text(booking['booking_user']['name']),
+                      title: Text(booking['booking_user']['name']??"Unnamed"),
                     
                       subtitle: Text(booking['material_type']),
                     
