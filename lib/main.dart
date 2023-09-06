9f030003ce24ffa21c83adb7c0bec0a9b3b958bc
RajuHaldar <rajuhaldarx@gmail.com>
2023-09-06 04:25:12 +0100

    hh

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,3 @@
-import 'package:car_booking/DriverDashboard.dart';
-import 'package:car_booking/UploadDashboard.dart';
-import 'package:car_booking/UserDasboard.dart';
-import 'package:car_booking/booking.dart';
-import 'package:car_booking/drftest.dart';
-import 'package:car_booking/driver_profile.dart';
-import 'package:car_booking/owner_profile.dart';
-import 'package:car_booking/profile.dart';
-import 'package:car_booking/rloginn.dart';
-import 'package:car_booking/front.dart';
-import 'package:car_booking/lorry.dart';
-import 'package:car_booking/otp_verify.dart';
-import 'package:car_booking/rough.dart';
-import 'package:car_booking/sign_in.dart';
-import 'package:car_booking/upload.dart';
-import 'package:car_booking/uploadx.dart';
 import 'package:flutter/material.dart';
 
 import 'map_with_autocomplete.dart';
@@ -30,9 +14,9 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         theme: ThemeData(scaffoldBackgroundColor: const Color(0xfff8fbff)),
-        home: const UserDashBoard());
+        home:  Yix());
 
-        
+        // A
 
     //
   }
@@ -74,119 +58,6 @@ child:  ListTile(),
 
 
 
-// child: ListView.builder(
-//   itemCount: xlist.length,
-  
-  
-//   itemBuilder: (BuildContext context, index) {
-  
-// return 
-
-// InkWell(
-//   onTap: () {
-   
-//    setState(() {
-//      xlist.add("QT");
-//    });
-   
-
-//   },
-//   child:   SizedBox(
-  
-//     height: 80,
-  
-//     child: 
-  
-    
-  
-    
-  
-//       Row(
-  
-    
-  
-    
-  
-  
-  
-//       children: [
-  
-//     const SizedBox(width: 10,),
-  
-//         const Image(image: AssetImage("img/QT.jpeg"),width: 100,height: 100,),
-  
-  
-  
-//            Column(
-  
-//             crossAxisAlignment: CrossAxisAlignment.start,
-  
-//             children: [
-  
-        
-  
-        
-  
-//             const Text("Tata LPT 25211 Truck",style: TextStyle(fontSize: 20)),
-  
-//             Text("${xlist[index]}",style: const TextStyle(fontSize: 20),),
-  
-//             const Text("★ 4.1 (100)",style: TextStyle(fontSize: 20),)
-  
-        
-  
-//           ],)
-  
-     
-  
-//     ,
-  
-//       const SizedBox(width: 20,),
-  
-  
-  
-//      const Column(
-  
-//             crossAxisAlignment: CrossAxisAlignment.start,
-  
-//             children: [
-  
-        
-  
-        
-  
-//             Text("Price",style: TextStyle(fontSize: 20)),
-  
-//             Text("Raju Haldar",style: TextStyle(fontSize: 20),),
-  
-//             Text("★ 4.1 (100)",style: TextStyle(fontSize: 20),)
-  
-        
-  
-//           ],),
-  
-//       ],
-  
-    
-  
-//     ),
-  
-  
-  
-  
-  
-//   ),
-// )
-
-//  ;
-// }
-  
-  
-  
-  
-    
-  
-//   ),
 
 )
 );
